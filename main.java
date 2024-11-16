861d97ce3ff0743bbe442330e3c47d94a7371a18
ietzel <67884277+ietzel@users.noreply.github.com>
2024-11-16 12:37:44 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,1 @@
+System.out.println("These are useful geometric numbers:"   putStrLn ("Tetrahedral, Square Pyramidal, Triangular Bipyramid, Triangular Prism, Octahedral, Cubic; " ++ show [1, 4, 5, 6, 8, 10, 12, 14, 20, 27, 30, 34, 35, 42, 54, 55, 56, 64]));
